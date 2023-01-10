5b8009aa7c26e114eeacbe6771641f069451d771
Dmitriy <sheleg.dmitriy@gmail.com>
2023-01-10 22:17:09 +0200

    Fix margin

--- frontend/components/ExplainText.tsx
+++ frontend/components/ExplainText.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 
 const ExplainText = () => (
-  <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
+  <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10 mt-8">
     <div className="mx-auto max-w-md">
       <div className="divide-y divide-gray-300/50">
         <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
