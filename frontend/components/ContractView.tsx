97692544b9591b93e7342048d655c021c788c26e
Dmitriy <34593263+shelegdmitriy@users.noreply.github.com>
2023-01-10 22:27:13 +0200

    Fix margin (#5)

--- frontend/components/ContractView.tsx
+++ frontend/components/ContractView.tsx
@@ -54,7 +54,7 @@ const ContractView = ({ contractName }: Props) => {
   }
 
   return (
-    <div className={`relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10 mb-8 ${uiPleaseWait ? 'opacity-25' : ''}`}>
+    <div className="relative bg-white px-6 pt-10 pb-8 shadow-xl ring-1 ring-gray-900/5 sm:mx-auto sm:max-w-lg sm:rounded-lg sm:px-10">
       <div className="mx-auto max-w-md">
         <div className="divide-y divide-gray-300/50">
           <div className="space-y-6 py-8 text-base leading-7 text-gray-600">
@@ -79,9 +79,9 @@ const ContractView = ({ contractName }: Props) => {
                       <circle className="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" strokeWidth="4"></circle>
                       <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
                     </svg>
-                  ) : null}
-                  <span>Save</span>
-                  <div className="loader"></div>
+                  ) : (
+                    <span>Save</span>
+                  )}
                 </button>
               </div>
             </form>
